7dba8077faca34260fc43305bc7e31daec21b735
Gregg Mojica <greggmojica@Greggs-Macbook-Pro.local>
2018-12-20 15:04:49 -0500

    add link

--- src/Home.js
+++ src/Home.js
@@ -11,19 +11,21 @@ class Home extends Component {
          <div>
            <br />
            <div className="container">
-             <h2>Food for sale</h2>
+             <h2>Online Marketplace</h2>
              <hr />
               <div className="row">
-                <div className="col-4 ">
-                  <img src="https://cdn.bfloschool.com/projects/marketplace/images/smoothie.jpg" className="cover" /><br />
-                  <span><b>Grilled Cheese</b> $20</span><br />
-                  <small className="gray">This is a cheap grilled cheese</small><br />
+                  <div className="col-4 ">
+                  <a href="/item">
+                    <img src="https://cdn.bfloschool.com/projects/marketplace/images/smoothie.jpg" className="cover" /><br />
+                    <span><b>Smoothie</b> $20</span><br />
+                    <small className="gray">Yummy smoothie!</small><br />
+                  </a>
                 </div>
 
                 <div className="col-4 ">
                   <img src="https://cdn.bfloschool.com/projects/marketplace/images/hamburger.jpg" className="cover" /><br />
-                  <span><b>Grilled Cheese</b> $20</span><br />
-                  <small className="gray">This is a cheap grilled cheese</small><br />
+                  <span><b>Hamburger</b> $20</span><br />
+                  <small className="gray">Delicious veggie burger!</small><br />
                 </div>
 
                 <div className="col-4 ">
