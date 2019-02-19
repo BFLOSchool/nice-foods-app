bddedfcdc637069186a4133a8938445a2cda8ebe
Gregg Mojica <greggmojica@Greggs-Macbook-Pro.local>
2019-02-19 15:04:39 -0500

    authentication

---
+++ src/Login.js
@@ -0,0 +1,73 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import Nav from './Components/Nav';
+import axios from 'axios'
+
+class Login extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleEmailChange = this.handleEmailChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+  handleEmailChange(event) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+  handlePasswordChange(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+  handleSubmit(event) {
+    if (this.state.email != "" && this.state.password != "") {
+      var data = {
+        email: this.state.email,
+        password: this.state.password
+      }
+      axios.post('http://localhost:3000/login', data).then(response =>{
+
+      }).catch(error =>{
+        alert('Whoops, something went wrong. Please try again!')
+      });
+    } else {
+      alert("soething went wrong")
+    }
+  }
+  render() {
+    return (
+      <div style={{backgroundColor: '#FAFAFA', height: 1000}}>
+        <Nav />
+        <div className="container">
+          <div className="row">
+            <div className="col-12" style={{backgroundColor: 'white', padding: 20}}>
+              <h4><b>Login to your account</b></h4>
+              <hr />
+              <form onSubmit={this.state.handleSubmit}>
+                <div className="form-group">
+                  <label>Email Address</label>
+                  <input onChange={this.state.handleEmailChange} type="email" className="form-control" placeholder="Enter email address" />
+                  <small className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input onChange={this.state.handlePasswordChange} type="password" className="form-control" placeholder="Password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
